d2f85853fc60448bc8625283f2ed28448f964d2b
kushpandey3 <165596137+kushpandey3@users.noreply.github.com>
2026-08-01 15:00:59 -0400

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        List<List<Integer>> adjacencies = new ArrayList<>(n);
+        for(int i = 0; i < n; i++) adjacencies.add(new ArrayList<Integer>());
+        for(int i = 0; i < edges.length; i++){
+            adjacencies.get(edges[i][0]).add(edges[i][1]);
+            adjacencies.get(edges[i][1]).add(edges[i][0]);
+        }
+        boolean[] visited = new boolean[n];
+        int ret = 0;
+        for(int i = 0; i < adjacencies.size(); i++){
+            if(!visited[i]){
+                ret++;
+                dfs(adjacencies, visited, i);
+            }
+        }
+        return ret;
+    }
+    public void dfs(List<List<Integer>> adjacencies, boolean[] visited, int node){
+        if(visited[node]) return; 
+        visited[node] = true; 
+        for(int neighbor : adjacencies.get(node)) dfs(adjacencies, visited, neighbor);
+    }
+}
