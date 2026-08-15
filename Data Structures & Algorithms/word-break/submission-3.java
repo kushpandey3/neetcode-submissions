a00f66b7733941afe574725ce3c56118187692c7
kushpandey3 <165596137+kushpandey3@users.noreply.github.com>
2026-08-15 09:47:35 -0400

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return helper(s, wordDict, 0, new Boolean[s.length()]);
+    }
+    public boolean helper(String s, List<String> wordDict, int startIndex, Boolean[] memo){
+        if(startIndex>=s.length()) return true;
+        if(memo[startIndex]!=null) return memo[startIndex];
+        for(String str : wordDict){
+            if(s.startsWith(str, startIndex)){
+                memo[startIndex] = helper(s, wordDict, str.length() + startIndex, memo);
+                if(memo[startIndex]) return true;
+            }
+        }
+        memo[startIndex] = false;
+        return false;
+    }
+}
